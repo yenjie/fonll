68a1a4655c14f62a878a907701f8dfa2fb39516c
Yen-Jie Lee <yenjie@gmail.com>
2019-08-28 11:02:35 -0400

    update

--- rebin.C
+++ rebin.C
@@ -35,8 +35,22 @@ void rebin()
 
    TCanvas *c5TeV = new TCanvas("c5TeV","",600,600);
    TCanvas *c7TeV = new TCanvas("c7TeV","",600,600);
-   TFile *infData = new TFile("data/HEPData-ins914325-v1-Table_2.root");
+   TCanvas *cBp7TeV = new TCanvas("cBp7TeV","",600,600);
    
+   TFile *infData = new TFile("data/HEPData-ins914325-v1-Table_2.root");   // CMS 7 TeV Bs
+//   TFile *infData = new TFile("data/HEPData-ins883318-v1-Table_2.root");   // CMS 7 TeV B+
+   
+   const int NbinBp7TeV = 5;
+   double binsBp7TeV[NbinBp7TeV+1] = {5,10,13,17,24,30};
+   TH1D *hDataBp7TeV = new TH1D("hDataBp7TeV","",NbinBp7TeV,binsBp7TeV);
+
+   hDataBp7TeV->SetBinContent(1,4.07); hDataBp7TeV->SetBinError(1,0.563028);
+   hDataBp7TeV->SetBinContent(2,1.47); hDataBp7TeV->SetBinError(2,0.158114);
+   hDataBp7TeV->SetBinContent(3,0.412); hDataBp7TeV->SetBinError(3,0.0485489);
+   hDataBp7TeV->SetBinContent(4,0.181); hDataBp7TeV->SetBinError(4,0.0192094);
+   hDataBp7TeV->SetBinContent(5,0.042); hDataBp7TeV->SetBinError(5,0.00806226);
+
+
    TH1D *hData7TeV   = (TH1D*) infData->Get("Table 2/Hist1D_y1");
    TH1D *hDataE1 = (TH1D*) infData->Get("Table 2/Hist1D_y1_e1");  // stat
    TH1D *hDataE2 = (TH1D*) infData->Get("Table 2/Hist1D_y1_e2");  // syst
@@ -69,6 +83,7 @@ void rebin()
       double error = sqrt( hDataE1->GetBinContent(i)*hDataE1->GetBinContent(i) +
                            hDataE2->GetBinContent(i)*hDataE2->GetBinContent(i) ); 
       hData7TeV->SetBinError(i,error);
+      cout <<"hData7TeV->SetBinContent("<<i<<","<<hData7TeV->GetBinContent(i)<<"); hData7TeV->SetBinError("<<i<<","<<error<<");"<<endl;
    }
    hData5TeV->SetMarkerStyle(20);
    hData7TeV->SetMarkerStyle(20);
@@ -77,15 +92,23 @@ void rebin()
    hData5TeV->SetXTitle("B_{s} [GeV]");
    hData5TeV->SetYTitle("d#sigma/dp_{T} [pb/GeV]");
    hData7TeV->Scale(1000);
+   hDataBp7TeV->SetXTitle("B^{+} [GeV]");
+   hDataBp7TeV->SetYTitle("d#sigma/dp_{T} [#mu b/GeV]");
    hData7TeV->SetLabelSize(hData5TeV->GetLabelSize("X"),"X");
    hData7TeV->SetLabelSize(hData5TeV->GetLabelSize("Y"),"Y");
    hData7TeV->SetTitleSize(hData5TeV->GetTitleSize("X"),"X");
    hData7TeV->SetTitleSize(hData5TeV->GetTitleSize("Y"),"Y");
+   hDataBp7TeV->SetLabelSize(hData5TeV->GetLabelSize("X"),"X");
+   hDataBp7TeV->SetLabelSize(hData5TeV->GetLabelSize("Y"),"Y");
+   hDataBp7TeV->SetTitleSize(hData5TeV->GetTitleSize("X"),"X");
+   hDataBp7TeV->SetTitleSize(hData5TeV->GetTitleSize("Y"),"Y");
 
    c5TeV->cd();   
    hData5TeV->Draw();
    c7TeV->cd();   
    hData7TeV->Draw();
+   cBp7TeV->cd();   
+   hDataBp7TeV->Draw();
    
  
    TFile *inf5TeV = new TFile("data/BmesonFONLL5TeV.root");
@@ -97,33 +120,45 @@ void rebin()
    TH1D *hout5TeV[10];
    TH1D *h7TeV[10];
    TH1D *hout7TeV[10];
+   TH1D *hBp7TeV[10];
+   TH1D *houtBp7TeV[10];
 
     for (int i=1;i<=9;i++) {
        c5TeV->cd();
        h5TeV[i] = (TH1D*)inf5TeV->Get(Form("h%d",i));
        h5TeV[i]->Scale(0.11);
-       hout5TeV[i] = rebinHist(h5TeV[i],hData5TeV,Form("hout%d",i));
+       hout5TeV[i] = rebinHist(h5TeV[i],hData5TeV,Form("hout5TeV%d",i));
        hout5TeV[i]->SetLineColor(color[i]);
        hout5TeV[i]->Draw("same hist");
        c7TeV->cd();
        h7TeV[i] = (TH1D*)inf7TeV->Get(Form("h%d",i));
        h7TeV[i]->Scale(0.11);
-       hout7TeV[i] = rebinHist(h7TeV[i],hData7TeV,Form("hout%d",i));
+       hout7TeV[i] = rebinHist(h7TeV[i],hData7TeV,Form("hout7TeV%d",i));
        hout7TeV[i]->SetLineColor(color[i]);
        hout7TeV[i]->Draw("same hist");
+       cBp7TeV->cd();
+       hBp7TeV[i] = (TH1D*)inf7TeV->Get(Form("h%d",i));
+       hBp7TeV[i]->Scale(0.403);
+       houtBp7TeV[i] = rebinHist(hBp7TeV[i],hDataBp7TeV,Form("houtBp7TeV%d",i));
+       houtBp7TeV[i]->SetLineColor(color[i]);
+       houtBp7TeV[i]->Draw("same hist");
     }
 
     TLegend *leg5TeV = new TLegend(0.5,0.5,0.9,0.9);
     TLegend *leg7TeV = new TLegend(0.5,0.5,0.9,0.9);
+    TLegend *legBp7TeV = new TLegend(0.5,0.5,0.9,0.9);
     
     for (int i=1;i<=9;i++){
        leg5TeV->AddEntry(hout5TeV[i],Form("%s,%s",hout5TeV[i]->GetTitle(),titles[i].c_str()),"l");
        leg7TeV->AddEntry(hout7TeV[i],Form("%s,%s",hout7TeV[i]->GetTitle(),titles[i].c_str()),"l");
+       legBp7TeV->AddEntry(houtBp7TeV[i],Form("%s,%s",houtBp7TeV[i]->GetTitle(),titles[i].c_str()),"l");
     }
     c5TeV->cd();
     leg5TeV->Draw();
     c7TeV->cd();
     leg7TeV->Draw();
+    cBp7TeV->cd();
+    legBp7TeV->Draw();
     c5TeV->SaveAs("Results_5TeV.png");
     c5TeV->SetLogy();
     c5TeV->Update();
@@ -132,4 +167,8 @@ void rebin()
     c7TeV->SetLogy();
     c7TeV->Update();
     c7TeV->SaveAs("Results_7TeV_log.png");
+    cBp7TeV->SaveAs("Results_Bp7TeV.png");
+    cBp7TeV->SetLogy();
+    cBp7TeV->Update();
+    cBp7TeV->SaveAs("Results_Bp7TeV_log.png");
 }
